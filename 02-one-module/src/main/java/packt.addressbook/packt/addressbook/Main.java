50a5d906c13437ae2a682775dfd05a9f895572ec
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2021-05-01 17:12:15 -0300

    Create another module

--- 02-one-module/src/main/java/packt.addressbook/packt/addressbook/Main.java
+++ 02-one-module/src/main/java/packt.addressbook/packt/addressbook/Main.java
@@ -1,8 +1,17 @@
 package packt.addressbook; 
 
-public class Main { 
+import java.util.*;
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+import packt.addressbook.model.*;
+import packt.addressbook.util.*;
+
+public class Main {  
+
+	public static void main(String[] args) { 
+		ContactUtil contactUtil = new ContactUtil(); 
+		SortUtil sortUtil = new SortUtil(); 
+		List<Contact> contacts = contactUtil.getContacts(); 
+		sortUtil.sortList(contacts); 
+		System.out.println(contacts); 
 	}
-}
+} 
