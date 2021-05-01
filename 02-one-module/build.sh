43a26c6332a286d1c44cd235c534a99f2721769b
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2021-05-01 17:16:30 -0300

    Simplify compile build.sh file

--- 02-one-module/build.sh
+++ 02-one-module/build.sh
@@ -1,6 +1,1 @@
-javac --module-source-path src/main/java -d out \
-src/main/java/packt.addressbook/packt/addressbook/Main.java \
-src/main/java/packt.addressbook/packt/addressbook/model/Contact.java \
-src/main/java/packt.addressbook/packt/addressbook/util/ContactUtil.java \
-src/main/java/packt.addressbook/packt/addressbook/util/SortUtil.java \
-src/main/java/packt.addressbook/module-info.java
+javac --module-source-path src/main/java -d out --module packt.addressbook
