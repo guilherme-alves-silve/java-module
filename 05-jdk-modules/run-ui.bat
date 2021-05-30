1a08d1fd3ef3dbeaa3252455a3f8b39f0fd3d90c
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2021-05-30 13:49:12 -0300

    Update files

--- 05-jdk-modules/run-ui.bat
+++ 05-jdk-modules/run-ui.bat
@@ -1,2 +1,3 @@
-set PATH_TO_FX="D:\javafx-sdk-11.0.2\lib"
+@REM #https://openjfx.io/openjfx-docs/#install-javafx
+set PATH_TO_FX="C:\path\to\javafx-sdk-version\lib"
 java --module-path %PATH_TO_FX%;out --add-modules javafx.controls --module packt.addressbook.ui/packt.addressbook.ui.Main
--- 05-jdk-modules/run-ui.sh
+++ 05-jdk-modules/run-ui.sh
@@ -1,2 +1,3 @@
-export PATH_TO_FX="D:\javafx-sdk-11.0.2\lib"
+#https://openjfx.io/openjfx-docs/#install-javafx
+export PATH_TO_FX="/path/to/javafx-sdk-version/lib"
 java --module-path $PATH_TO_FX:out --add-modules javafx.controls --module packt.addressbook.ui/packt.addressbook.ui.Main
