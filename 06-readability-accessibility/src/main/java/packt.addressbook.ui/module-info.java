2ff685d104d613a5e2e8a2e578dc3f5e390be47d
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2021-06-01 00:00:10 -0300

    Export the packt.addressbook.ui just to the module javafx.graphics, so it can become encapsulated

--- 06-readability-accessibility/src/main/java/packt.addressbook.ui/module-info.java
+++ 06-readability-accessibility/src/main/java/packt.addressbook.ui/module-info.java
@@ -1,5 +1,5 @@
 module packt.addressbook.ui {
-    exports packt.addressbook.ui; 
+    exports packt.addressbook.ui to javafx.graphics; 
     requires java.logging;
     requires javafx.controls;
     requires packt.addressbook.lib;
