b3aa5e60887706fc0bf160692738904dc9658197
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2021-06-14 01:30:56 -0300

    Run application with jlink

--- 08-module-with-jlink/build.bat
+++ 08-module-with-jlink/build.bat
@@ -4,4 +4,5 @@
 set PATH_TO_FX="D:\javafx-sdk-11.0.2\lib"
 set PATH_TO_FX_MODS="D:\javafx-sdk-11.0.2\jmods"
 javac --module-source-path src\main\java\ --module-path %PATH_TO_FX% --add-modules javafx.controls -d out/ --module packt.addressbook,packt.addressbook.ui,packt.sortutil,packt.contact,packt.sort.bubblesort,packt.sort.javasort
-%JAVA_HOME%/bin/jlink --module-path out;%PATH_TO_FX_MODS% --add-modules javafx.controls --output image
+%JAVA_HOME%/bin/jlink --module-path out/;%PATH_TO_FX_MODS% --add-modules packt.addressbook.ui --bind-services --output image
+cmd /c echo F | xcopy input.xml image\
--- 08-module-with-jlink/run-jlink.bat
+++ 08-module-with-jlink/run-jlink.bat
@@ -1,4 +1,4 @@
 @REM #https://openjfx.io/openjfx-docs/#install-javafx
 @REM set PATH_TO_FX="C:\path\to\javafx-sdk-version\lib"
-set PATH_TO_FX="D:\javafx-sdk-11.0.2\lib"
-jlink --module-path %PATH_TO_FX%;out --add-modules javafx.controls --module packt.addressbook.ui/packt.addressbook.ui.Main
+cd image/
+bin\java --add-modules javafx.controls --module packt.addressbook.ui/packt.addressbook.ui.Main
